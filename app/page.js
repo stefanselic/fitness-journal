a4576c9ef4ae77c2af530b9616f06e167771dba7
stefanselic <s.stefan22@hotmail.com>
2023-06-15 15:46:37 +0200

    Create landing page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,31 @@
-export default function Home() {
-  return <main></main>;
+import Image from 'next/image';
+
+import gymStats from '../public/images/fitness-stats.png';
+import styles from './page.module.scss';
+
+export default function LandingPage() {
+  return (
+    <main className={styles.mainContainer}>
+      <div>
+        <div className={styles.title}>
+          <p>Fitness Journal </p>
+        </div>
+        <div>
+          <Image
+            className={styles.image}
+            src={gymStats}
+            alt="man running illustration"
+          />
+        </div>
+        <div>
+          <p>Get Started Now </p>
+        </div>
+        <div className={styles.buttonContainer}>
+          <button>Login</button>
+          <button>Sign up</button>
+        </div>
+      </div>
+      <div className={styles.colorContainer} />
+    </main>
+  );
 }
