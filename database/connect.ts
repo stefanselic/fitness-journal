cb8c508ecc7b4cfc289e1f76659566154da9cdc6
stefanselic <s.stefan22@hotmail.com>
2023-07-08 20:19:40 +0200

    Update connect

--- database/connect.ts
+++ database/connect.ts
@@ -9,7 +9,7 @@ declare module globalThis {
   let postgresSqlClient: Sql;
 }
 
-// Connect only once to the databasee
+// Connect only once to the database
 // https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
 function connectOneTimeToDatabase() {
   if (!('postgresSqlClient' in globalThis)) {
